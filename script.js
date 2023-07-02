7cac48722cdc06be261aa801252b07870e5414ed
Harshad Sonar <61082703+Harshadsonar@users.noreply.github.com>
2023-07-02 17:59:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
-window.promises = [];
+const promises = [];
+
+function getRandomDelay() {
+	return Math.floor(Math.random()*5)+1;
+}
+for(let i=0; i<5; i++){
+	const promise = new Promise((resolve) => {
+		const delay = getRandomDelay();
+		setTimeout(() => {
+			resolve(`Promise ${i + 1} resolved after ${delay} seconds`);
+		},delay*1000);
+	});
+	promises.push(promise);
+
+	Promise.any(promises)
+	.then((result) => {
+		co
+	})
+}
 
 // Do not change the code above this
 // add your promises to the array `promises`

14c0afa685254d42c6197b125d51ef8846c6bf71
Harshad Sonar <61082703+Harshadsonar@users.noreply.github.com>
2023-07-02 18:21:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,24 @@
 const promises = [];
 
 function getRandomDelay() {
-	return Math.floor(Math.random()*5)+1;
-}
-for(let i=0; i<5; i++){
-	const promise = new Promise((resolve) => {
-		const delay = getRandomDelay();
-		setTimeout(() => {
-			resolve(`Promise ${i + 1} resolved after ${delay} seconds`);
-		},delay*1000);
-	});
-	promises.push(promise);
-
-	Promise.any(promises)
-	.then((result) => {
-		const outputDiv = document.getElementById('output');
-		outputDiv.textContent = result;
-	})
-	.catch((error) => {
-		console.error(error);
-	});
+  return Math.floor(Math.random() * 5) + 1;
 }
 
-// Do not change the code above this
-// add your promises to the array `promises`
+for (let i = 0; i < 5; i++) {
+  const promise = new Promise((resolve) => {
+    const delay = getRandomDelay();
+    setTimeout(() => {
+      resolve(`Promise ${i + 1} resolved after ${delay} seconds`);
+    }, delay * 1000);
+  });
+  promises.push(promise);
+}
+
+Promise.any(promises)
+  .then((result) => {
+    const outputDiv = document.getElementById('output');
+    outputDiv.textContent = result;
+  })
+  .catch((error) => {
+    console.error(error);
+  });

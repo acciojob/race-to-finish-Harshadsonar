60c9bfdf915cbb384e8931c71b78f33c84eacd46
Harshad Sonar <61082703+Harshadsonar@users.noreply.github.com>
2023-07-03 18:02:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,34 +1,11 @@
-let a = new Promise(resolve => {
-	setTimeout(() => {
-		resolve("a");
-	}, 2000)
-})
-let b = new Promise(resolve => {
-	setTimeout(() => {
-		resolve("b");
-	}, 3000)
-})
-let c = new Promise(resolve => {
-	setTimeout(() => {
-		resolve("c");
-	}, 1000)
-})
-let d = new Promise(resolve => {
-	setTimeout(() => {
-		resolve("d");
-	}, 4000)
-})
-let e = new Promise(resolve => {
-	setTimeout(() => {
-		resolve("e");
-	}, 5000)
-})
-
-let x = Promise.any{[a, b, c, d, e]}
-
-window.promises = [a, b, c, d, e];
-
-x.then((data) => {
+function generatePromise() {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			resolve("Hello, world!")
+		},1000)
+	})
+}
+generatePromise().then(data) => {
 	const element = document.getElementById("output");
 	element.innerText = data;
-})
+}
